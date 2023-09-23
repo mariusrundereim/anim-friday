49f5e28862b112b260887ebe76a71e032820fb38
mariusrundereim <marius.rundereim@gmail.com>
2023-09-23 14:55:13 +0200

    more script functions

--- js/script.js
+++ js/script.js
@@ -13,10 +13,39 @@ btnClear.addEventListener("click", (e) => {
 
 // Main Canvas
 const canvas = document.querySelector("#artboard");
+
+const canWidth = 550;
 canvas.width = 550;
 canvas.height = 850;
 
 const context = canvas.getContext("2d");
+
+// Player input
+
+let playerInput = 0;
+let currentKeystroke = "";
+const distance = 5;
+
+// left ArrowLeft
+// right ArrowRight
+// a KeyA
+// d KeyD
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft" || e.code === "KeyA") {
+    playerInput = -distance;
+    currentKeystroke = e.code;
+  } else if (e.code === "ArrowRight" || e.code === "KeyD") {
+    playerInput = distance;
+    currentKeystroke = e.code;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.code === currentKeystroke) {
+    playerInput = 0;
+  }
+});
 
 function drawBackground() {
   // Bg
@@ -36,6 +65,33 @@ function drawBackground() {
 }
 
 function clear() {
-  //
   canvas.clearRect(0, 0, 600, 400);
 }
+
+function drawPlayer() {
+  const { color, x, y, width, height } = player;
+
+  player.fillStyle = color;
+}
+
+const player = {
+  x: 0,
+  y: 300,
+  width: 20,
+  height: 50,
+  color: "black",
+};
+
+// let i = 0;
+// setInterval(() => {
+//   player.x = Number(player.x) + playerInput;
+//   if (player.x > canWidth - player.width) {
+//     player.x = canWidth - player.width;
+//   } else if (player.x < 0) {
+//     player.x = 0;
+//   }
+//   clear();
+//   drawBackground();
+//   drawPlayer();
+//   i + i + 0.1;
+// }, 20);
